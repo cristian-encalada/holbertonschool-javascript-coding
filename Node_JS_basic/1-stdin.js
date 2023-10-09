c0c6b9790eee4d7e959c1a190c2ec6a0dbbe2d96
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-09 20:28:41 -0300

    [1] Fix: Use rl.on(line) instead of rl.question()

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,8 +7,8 @@ const rl = readline.createInterface({
 });
 
 console.log('Welcome to Holberton School, what is your name?');
-// rl.question() method displays the query by writing it to the output and waits for user input
-rl.question('', (name) => {
+// rl.on('line', ...) handle each line of input separately to work with both \n and \r line ends
+rl.on('line', (name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();
