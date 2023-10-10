7f3a288a2cdde0a55fe47816839110c4eef9a57d
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-10 14:58:10 -0300

    Minor improvement to server.listen on port 1245

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,12 +2,16 @@
 const http = require('http');
 
 // Create an HTTP server
-http.createServer((request, response) => {
+const server = http.createServer((request, response) => {
   // Send the status code 200 (success)
   response.statusCode = 200;
   // Send a text as the response body to the client
   response.write('Hello Holberton School!');
   // End of the response. No more data can be written
   response.end();
-  // The server is listening on port 1245 for incoming HTTP requests
-}).listen(1245);
+});
+
+// Listen on port 1245 for incoming HTTP requests
+server.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
