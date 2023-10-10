e94cc6d9b7ec1b7e01afea6acab64a7fe5b52b35
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-10 14:39:37 -0300

    [4] Simple HTTP server using Node HTTP module, done

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+// Import the HTTP Module
+const http = require('http');
+
+// Create an HTTP server
+http.createServer((request, response) => {
+  // Send a text as the response body to the client
+  response.write('Hello Holberton School!');
+  // End of the response. No more data can be written
+  response.end();
+  // The server is listening on port 1245 for incoming HTTP requests
+}).listen(1245);
