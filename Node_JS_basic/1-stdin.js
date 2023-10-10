883093c1fed13f3093c15101b866ca4eedda6dcd
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-10 12:19:36 -0300

    [1] Fix: Ensure to process the input only once

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,15 @@
 console.log('Welcome to Holberton School, what is your name?');
 
+process.stdin.setEncoding('utf8'); // Ensure that the input is a string
+
+let nameReceived = false; // Ensure to process the input only once
+
+// if nameReceived = false, process the input
+
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); // Remove extra newline
+  const name = data.trim(); // Remove extra newline
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   process.exit();
+  nameReceived = true;
 });
