93146dddf74ad5e3e8765d224023985dfb25a2e2
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-10 12:10:03 -0300

    [1] Alternative way, not using readline.createInterface()

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,8 @@
-const readline = require('readline');
-
-// https://nodejs.org/api/readline.html#readlinepromisescreateinterfaceoptions
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
-// rl.on('line', ...) handle each line of input separately to work with both \n and \r line ends
-rl.on('line', (name) => {
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Remove extra newline
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
-  rl.close();
+  process.exit();
 });
