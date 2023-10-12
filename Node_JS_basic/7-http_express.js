52ebc98489aeb2d1cb850ca83061bf382c7e1faa
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-12 17:12:16 -0300

    [7] Revert last changes.

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,13 +1,6 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
-const fs = require('fs');
-
 const app = express();
-const port = 1245;
-
-// Get the database file name from the command-line arguments
-const commandLineArgs = process.argv.slice(2);
-const defaultDatabaseFile = commandLineArgs[0] || 'database.csv';
+const countStudents = require('./3-read_file_async');
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -15,13 +8,9 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res) => {
   try {
-    const databaseFile = req.query.file || defaultDatabaseFile;
-
-    console.log(`Received request for file: ${databaseFile}`);
+    const databaseFile = process.argv[2] || 'database.csv';
 
     let consoleOutput = '';
-
-    // Override the console.log function to capture output
     const originalConsoleLog = console.log;
     console.log = (message) => {
       consoleOutput += `${message}\n`;
@@ -33,16 +22,18 @@ app.get('/students', async (req, res) => {
 
     console.log = originalConsoleLog;
 
-    res.status(200).send(`This is the list of our students\n${consoleOutput}`);
+    res.send(`This is the list of our students\n${consoleOutput}`);
   } catch (error) {
     res.status(400).send(`This is the list of our students\n${error.message}`);
   }
 });
 
 app.use((req, res) => {
-  res.status(404).send('Not Found\n');
+  res.status(404).send('Not Found');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} using database file: ${defaultDatabaseFile}`);
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
 });
+
+module.exports = app;
