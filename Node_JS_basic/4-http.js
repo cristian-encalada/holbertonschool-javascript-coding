914b0ec3bdedd57fbb8e60b5d6f8c6f99a83a99c
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-12 13:36:30 -0300

    [4] Simplify code. Include export at the end

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,16 +2,12 @@
 const http = require('http');
 
 // Create an HTTP server
-const server = http.createServer((request, response) => {
-  // Send the status code 200 (success)
-  response.statusCode = 200;
+const server = http.createServer((_, response) => {
   // Send a text as the response body to the client
   response.write('Hello Holberton School!');
   // End of the response. No more data can be written
   response.end();
-});
-
-// Listen on port 1245 for incoming HTTP requests
-server.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+  // The server is listening on port 1245 for incoming HTTP requests
+}).listen(1245);
+// Export the 'server' object
+module.exports = server;
