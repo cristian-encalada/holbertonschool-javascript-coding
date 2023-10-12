84509c9b0942330b13ecf8ce18aab9a3468190c9
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-12 15:13:13 -0300

    [5] More complext HTPP server, done

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,40 +1,42 @@
-// Import the required modules
 const http = require('http');
 const url = require('url');
-const fs = require('fs').promises;
 const countStudents = require('./3-read_file_async');
 
-// Create an HTTP server
 const app = http.createServer(async (req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
   if (parsedUrl.pathname === '/') {
     // For the root path ('/'), display "Hello Holberton School!"
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
   } else if (parsedUrl.pathname === '/students') {
     try {
-      const databaseFile = process.argv[2] || 'database.csv';
-      const studentData = await countStudents(databaseFile);
+      const databaseFile = process.argv[2] || 'database.csv'; // // Get the database file path 
 
-      // Set the response content type and send the student data
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(`This is the list of our students\n${studentData}`);
+      let consoleOutput = '';  // variable to capture console.log output
+      const originalConsoleLog = console.log;
+      console.log = (message) => {
+        consoleOutput += message + '\n';  //  // Capture console.log output
+      };
+      
+      await countStudents(databaseFile); // Retrieve student data with countStudents() function
+
+      consoleOutput = consoleOutput.trim();  // // Remove extra newline from the end
+
+      console.log = originalConsoleLog; // // Restore the original console.log function
+
+      res.end(`This is the list of our students\n${consoleOutput}`);
     } catch (error) {
       // Handle errors by sending an error response
-      res.setHeader('Content-Type', 'text/plain');
       res.statusCode = 500;
       res.end('Internal Server Error\n');
     }
   } else {
     // Handle other paths with a "Not Found" response
-    res.setHeader('Content-Type', 'text/plain');
     res.statusCode = 404;
     res.end('Not Found\n');
   }
 });
 
-// Listen on port 1245
-app.listen(1245);
+app.listen(1245); // Listen on port 1245
 
 module.exports = app;
